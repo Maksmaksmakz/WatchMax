201bb293926790202b68c6472eb7e1ebb66ed16e
Maximilian Menschel <elmaxo@hotmail.de>
2017-06-22 14:34:37 +0200

    Add dist folder

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,50 @@
 "use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _http = require("http");
+
+var _http2 = _interopRequireDefault(_http);
+
+var _express = require("express");
+
+var _express2 = _interopRequireDefault(_express);
+
+var _bodyParser = require("body-parser");
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
+var _mongoose = require("mongoose");
+
+var _mongoose2 = _interopRequireDefault(_mongoose);
+
+var _config = require("./config");
+
+var _config2 = _interopRequireDefault(_config);
+
+var _userController = require("./controllers/userController");
+
+var _userController2 = _interopRequireDefault(_userController);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+app.server = _http2.default.createServer(app);
+
+//middleware
+app.use(_bodyParser2.default.json({
+  limit: _config2.default.bodyLimit
+}));
+
+app.use("/api/v1/users", _userController2.default);
+
+//v1 routes
+app.server.listen(_config2.default.port);
+console.log("server listening on: " + app.server.address().port);
+
+_mongoose2.default.connect(_config2.default.mongoUrl);
+
+exports.default = app;
 //# sourceMappingURL=index.js.map
