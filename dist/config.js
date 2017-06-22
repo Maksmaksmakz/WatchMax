84040f582fd45206ce19399eebbc618f493713a5
Maximilian Menschel <elmaxo@hotmail.de>
2017-06-22 16:22:26 +0200

    Remove build from start script and add dist folder

---
+++ dist/config.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = {
+  "port": 8005,
+  "mongoUrlDev": "mongodb://localhost:27017/MaxWatch-Api",
+  "mongoUrlProd": "mongodb://watch-max-db:7207de0ac3aca81e12e7967ff8b16e87@dokku-mongo-watch-max-db:27017/watch-max-db",
+  "bodyLimit": "100kb"
+};
+//# sourceMappingURL=config.js.map
